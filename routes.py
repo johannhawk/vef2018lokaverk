0d14f3d46a35ebd2372b3967cc3b326cb27f4cb7
johannhawk <johannsi223@nemi.tskoli.is>
2018-05-09 17:07:18 +0000

    Add files via upload

---
+++ routes.py
@@ -0,0 +1,28 @@
+from app import app
+from flask import render_template, url_for
+
+
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/tonlist')
+def tonlist():
+    return render_template('tonlist.html')
+
+@app.route('/gallery1')
+def gallery1():
+    return render_template('gallery1.html')
+
+@app.route('/gallery2')
+def gallery2():
+    return render_template('gallery2.html')
+
+@app.route('/bloghub')
+def bloghub():
+    return render_template('bloghub.html')
+
+@app.route()
